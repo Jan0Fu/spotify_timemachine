4b30093e0323db0af68aae1ed025c440a5d345ca
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-17 17:31:23 +0100

    Improved code, fixes

--- main.py
+++ main.py
@@ -37,5 +37,7 @@ for song in song_titles:
     except IndexError:
         print(f"{song} doesn't exist in Spotify, Skipped.")
 
+playlist = sp.user_playlist_create(user=user_id, name=f"{user_date} Billboard 100", public=False)
 
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
 
