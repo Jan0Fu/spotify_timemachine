c74b5ee0dbac9067880e5556707ba2241f67dd29
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-17 17:20:09 +0100

    Finished main functionality of program

--- main.py
+++ main.py
@@ -2,31 +2,40 @@ from bs4 import BeautifulSoup
 import os
 import requests
 import spotipy
-from spotipy.oauth2 import
+from spotipy.oauth2 import SpotifyOAuth
 
 client_id = os.environ.get("SPOTIPY_CLIENT_ID")
 client_secret = os.environ.get("SPOTPY_CLIENT_SECRET")
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id="58b8048b059d448894ebb94d99ccac28",
+        client_secret="3d56656c9ab34120b2f6019d642a8d54",
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+
 user_date = input("Which year do you want to travel to? Type the data in this format YYYY-MM-DD: ")
 response = requests.get("https://www.billboard.com/charts/hot-100/" + user_date)
+year = user_date.split("-")[0]
 
 soup = BeautifulSoup(response.text, "html.parser")
 best_songs = soup.find_all(name="h3", id="title-of-a-story", class_="a-no-trucate")
 song_titles = [song.getText().strip("\n\t") for song in best_songs]
 print(song_titles)
 
+song_uris = []
+for song in song_titles:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify, Skipped.")
 
-#article_upvotes = [int(score.getText().split()[0]) for score in soup.findAll("span", class_="score")]
 
 
-# with open("website.html") as file:
-#     contents = file.read()
-# soup = BeautifulSoup(contents, "html.parser")
-# all_anchors = soup.findAll(name="a")
-# print(all_anchors)
-# for anchor in all_anchors:
-#     print(anchor.get("href"))
-# heading = soup.find(name="h1", id="name")
-# company_url = soup.select_one("p a")
-# print(company_url)
-# headings = soup.select(".heading")
-# print((headings))
